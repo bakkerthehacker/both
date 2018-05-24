bcf1376572d88d318b29cb627d7c07b06d89e6f5
Grant Bakker <grant@bakker.pw>
2018-05-23 17:48:32 -0700

    setup env

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name='both',
+    version='0.0.1',
+    author='Grant Bakker',
+    author_email='grant@bakker.pw',
+    description='A small example package',
+    url='https://bakkerthehacker.github.io/both/',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ),
+)
