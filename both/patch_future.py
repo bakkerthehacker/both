87be1c07d7e07f9bbe0bb59c26328f186e266614
Grant Bakker <grant@bakker.pw>
2018-05-24 02:43:52 -0700

    add features correctly

--- both/patch_future.py
+++ both/patch_future.py
@@ -2,13 +2,18 @@
 import __future__
 
 something = (
-    'my_future'
+    'my_future',
 )
 
 
 def add_futures():
     for future_name in something:
-        feature = __future__._Feature(None, None, None)  # TODO
-        __future__.all_feature_names.append(future_name)
-        __future__.__all__.append(future_name)
-        setattr(__future__, future_name, feature)
+        if future_name not in __future__.all_feature_names:
+            feature = __future__._Feature(
+                (0, 0, 0, 'alpha',  0),
+                (0, 0, 0, 'alpha', 0),
+                0
+            )
+            __future__.all_feature_names.append(future_name)
+            __future__.__all__.append(future_name)
+            setattr(__future__, future_name, feature)
